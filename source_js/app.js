4bd271470d68fc80a86d9a0139bcf11461505ae5
Alec Fekete <alec.fekete@gmail.com>
2016-04-24 13:58:38 -0500

    Added bootstrap and pages

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,24 @@
-var app = angular.module('mp3',['ngRoute']);
+var app = angular.module('carpool',['ngRoute']);
 
 app.config(function ($routeProvider) {
-
+    $routeProvider
+        .when('/home', {
+            templateUrl : './partials/home.html',
+            controller : 'homeController'
+        })
+        .when('/find', {
+            templateUrl : './partials/find.html',
+            controller : 'findController'
+        })
+        .when('/drive', {
+            templateUrl : './partials/drive.html',
+            controller : 'driveController'
+        })
+        .when('/profile', {
+            templateUrl : './partials/profile.html',
+            controller : 'profileController'
+        })
+        .otherwise({
+            redirectTo: '/home'
+        })
 })
