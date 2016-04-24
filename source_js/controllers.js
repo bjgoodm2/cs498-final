d6a682be2592d92beb16296fd1047c06b3099262
Alec Fekete <alec.fekete@gmail.com>
2016-04-24 14:00:26 -0500

    Added controller placeholders

--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,1 +1,15 @@
-/* Sample Controller */
+app.controller('homeController', ['$scope', '$http', function($scope, $http) {
+
+}]);
+
+app.controller('findController', ['$scope', '$http', function($scope, $http) {
+
+}]);
+
+app.controller('driveController', ['$scope', '$http', function($scope, $http) {
+
+}]);
+
+app.controller('profileController', ['$scope', '$http', function($scope, $http) {
+
+}]);
