0e0002517cb316c1a9157f7395e2807e24d15020
pritz2 <pritzluke@gmail.com>
2016-04-25 15:45:27 -0500

    added js/css/public

---
+++ public/js/controllers.js
@@ -0,0 +1,1 @@
+app.controller("homeController",["$scope","$http",function($scope,$http){}]),app.controller("findController",["$scope","$http",function($scope,$http){}]),app.controller("driveController",["$scope","$http",function($scope,$http){}]),app.controller("profileController",["$scope","$http",function($scope,$http){}]);
