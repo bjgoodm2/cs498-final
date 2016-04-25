3c40bceb75a22f87acfc85909befe923ce388b3c
Alec Fekete <alec.fekete@gmail.com>
2016-04-25 18:08:27 -0500

    Added stuff back

---
+++ source_js/controllers.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Alec on 4/25/2016.
+ */
+app.controller('homeController', ['$scope', '$http', function($scope, $http) {
+    
+}]);
+
+app.controller('findController', ['$scope', '$http', function($scope, $http) {
+
+}]);
+
+app.controller('driveController', ['$scope', '$http', function($scope, $http) {
+
+}]);
+
+app.controller('profileController', ['$scope', '$http', function($scope, $http) {
+
+}]);
