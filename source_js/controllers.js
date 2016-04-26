20a58f34d802b426652ddc8741906f854ea41559
Alec Fekete <alec.fekete@gmail.com>
2016-04-25 23:14:32 -0500

    Added location autocomplete to find and drive pages

--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,13 +1,35 @@
 app.controller('homeController', ['$scope', '$http', function($scope, $http) {
-    
+
 }]);
 
 app.controller('findController', ['$scope', '$http', function($scope, $http) {
-
+    $scope.getLocation = function(val) {
+        return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+                address: val,
+                sensor: false
+            }
+        }).then(function(response){
+            return response.data.results.map(function(item){
+                return item.formatted_address;
+            });
+        });
+    };
 }]);
 
 app.controller('driveController', ['$scope', '$http', function($scope, $http) {
-
+    $scope.getLocation = function(val) {
+        return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+                address: val,
+                sensor: false
+            }
+        }).then(function(response){
+            return response.data.results.map(function(item){
+                return item.formatted_address;
+            });
+        });
+    };
 }]);
 
 app.controller('profileController', ['$scope', '$http', function($scope, $http) {
